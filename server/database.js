ffeb270e9382944d0c197ff1216314f9f60cc071
9900K <46859140+9900K@users.noreply.github.com>
2019-01-21 12:06:01 -0700

    Update database.js

--- server/database.js
+++ server/database.js
@@ -432,7 +432,7 @@ exports.addGiveaway = function(userId, callback) {
                     return callback({ message: 'NOT_ELIGIBLE', time: eligible});
                 }
 
-                var amount = 10000000000000000; // 2 bits
+                var amount = 50000; // 500 bits
                 client.query('INSERT INTO giveaways(user_id, amount) VALUES($1, $2) ', [userId, amount], function(err) {
                     if (err) return callback(err);
 
